2cd1e5cd05b2db899951baab30daf5d7b2e97810
soberspot <soberspot@gmail.com>
2020-02-03 23:00:45 +0300

    добавлен index.hrml в lending

--- yourexp/urls.py
+++ yourexp/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lending import views
 
 urlpatterns = [
+
+    path('', views.index, name='home'),
     path('admin/', admin.site.urls),
 ]
