6016794bb4d02ebec94ded56fd55d4e595330035
soberspot <soberspot@gmail.com>
2020-02-04 15:41:59 +0300

    тестовый шаблон со стилями

--- lending/views.py
+++ lending/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-# from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
-
-# def index(request):
-#     return HttpResponse("Hello World!")
--- yourexp/urls.py
+++ yourexp/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
 from lending import views
 
 urlpatterns = [
 
     path('', views.index, name='home'),
     path('admin/', admin.site.urls),
+    re_path(r'^admin', admin.site.urls),
 ]
