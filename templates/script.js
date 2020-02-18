f5735abfed04aba4963f6da60ba535750d666466
soberspot <soberspot@gmail.com>
2020-02-19 00:31:12 +0300

    18.02.2020

--- templates/script.js
+++
@@ -1,6 +0,0 @@
-/*
-
-emmet:
-div.table-grid>div.table-grid--container>div.table-grid--row>div.tg-col-6*2
-
-*/
--- yourexp/urls.py
+++ yourexp/urls.py
@@ -1,20 +1,5 @@
-"""yourexp URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.urls import re_path
 from lending import views
 
@@ -22,5 +7,7 @@ urlpatterns = [
 
     path('', views.index, name='home'),
     path('admin/', admin.site.urls),
-    re_path(r'^admin', admin.site.urls),
+    # re_path(r'^admin', admin.site.urls),
+    # path('about/', TemplateView.as_view(template_name="about.html")),
+    # path('contact/', TemplateView.as_view(template_name="contact.html")),
 ]
